e2de99785527e265f5a8422f2f82944846fca1e2
Bao <lhqbao1@gmail.com>
2026-01-20 11:09:55 +0700

    update

--- components/shared/trusted-shop.tsx
+++ components/shared/trusted-shop.tsx
@@ -7,6 +7,9 @@ import Script from "next/script";
 export function TrustedShops() {
   const pathname = usePathname();
 
+  if (pathname.includes("/anmelden") || pathname.includes("/einloggen")) {
+    return null;
+  }
   // ❌ Không load TrustedShops trong admin
 
   return (
@@ -16,12 +19,12 @@ export function TrustedShops() {
       strategy="lazyOnload"
       async
       data-desktop-position="left"
-      data-desktop-custom-width="156"
+      data-desktop-custom-width="157"
       data-desktop-disable-reviews="false"
       data-desktop-enable-custom="false"
       data-desktop-enable-fadeout="false"
       data-mobile-position="left"
-      data-mobile-custom-width="156"
+      data-mobile-custom-width="157"
       data-mobile-disable-reviews="false"
       data-mobile-enable-fadeout="true"
       data-disable-mobile="false"
