074ed50e1e37efde42721ac37e759aa6e71e8e5e
Bao <lhqbao1@gmail.com>
2026-01-30 18:02:36 +0700

    update

--- src/app/[locale]/(simple)/danke/container.tsx
+++ src/app/[locale]/(simple)/danke/container.tsx
@@ -220,7 +220,6 @@ const OrderPlaced = () => {
 
   //Trusted shop init
   useEffect(() => {
-    console.log(checkout);
     if (!checkout) return;
     if (trustedShopData) return; // ❗ chỉ set 1 lần
 
@@ -259,7 +258,6 @@ const OrderPlaced = () => {
 
     return () => clearTimeout(timer);
   }, [trustedShopData]);
-  console.log(trustedShopData);
 
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center gap-12 -translate-y-10 col-span-2">
